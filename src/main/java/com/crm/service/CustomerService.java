f5103c49c37c4c65c52a072c800b16e5361645f8
dwy <1554642647@qq.com>
2025-10-19 15:48:18 +0800

    feat(customer): 实现客户管理功能

--- src/main/java/com/crm/service/CustomerService.java
+++ src/main/java/com/crm/service/CustomerService.java
@@ -1,7 +1,14 @@
 package com.crm.service;
 
+import com.crm.common.result.PageResult;
 import com.crm.entity.Customer;
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.crm.query.CustomerQuery;
+import com.crm.vo.CustomerVO;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.net.http.HttpResponse;
+import java.rmi.ServerException;
 
 /**
  * <p>
@@ -12,5 +19,10 @@ import com.baomidou.mybatisplus.extension.service.IService;
  * @since 2025-10-12
  */
 public interface CustomerService extends IService<Customer> {
-
+//分页
+    PageResult<CustomerVO> getPage(CustomerQuery query);
+    //导出
+    void exportCustomer(CustomerQuery query, HttpServletResponse response);
+    //新增或修改
+    void saveOrUpdate(CustomerVO customerVO) throws ServerException;
 }
